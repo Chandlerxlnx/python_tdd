dad33807f511e0d73b2ffa85518dd86bcbf581c9
Chandler Zhao <zhao_chunxiao@hotmail.com>
2021-05-07 21:44:32 +0800

    rend to-do list in table. 1 item is correct

--- chapter01/superlists/function_test.py
+++ chapter01/superlists/function_test.py
@@ -35,7 +35,7 @@ class NewVistorTest(unittest.TestCase):
             )
         #在文本框输入”buy peacock feathers"
         # habit is fake flies fishing
-        inputbox.send_keys('buy peacock features')
+        inputbox.send_keys('Buy peacock feathers')
         
         #按回车，页面更新
         #代办事项显示“1.buy peacock feathers"
@@ -46,13 +46,24 @@ class NewVistorTest(unittest.TestCase):
         print(self.browser.find_elements_by_id('id*'))
         table = self.browser.find_element_by_id ("id_list_table")
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
-        )
+        #self.assertTrue(
+        #    any(row.text == '1: Buy peacock feathers' for row in rows) ,
+        #    f"New to-do item did not appear in table. Contents were:\n {table.text}"
+        #)
+        self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
 
         #页面又显示一个文件框，可以输入其他代办事项
         #输入”use peacock feathers to make a fly“
+        inputbox =self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
         
+        #按回车，页面更新
+        #代办事项显示“1.buy peacock feathers"
+        inputbox.send_keys(Keys.ENTER) 
+        time.sleep(1)
+        self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly',[row.text for row in rows])
+
         self.fail('Finish the test')
         #按回车，页面更新
         #代办事项显示 2个代办项
--- chapter01/superlists/lists/tests.py
+++ chapter01/superlists/lists/tests.py
@@ -32,8 +32,8 @@ class HommePageTest(TestCase):
         #assertTemplateUsed need work with client
         response = self.client.get('/')
         self.assertTemplateUsed(response,'home.html')
+
     def test_can_save_a_POST_request(self):
         response = self.client.post('/',data={'item_text':'A new list item'})
         self.assertIn('A new list item',response.content.decode())
-        self.assertTemplateUsed(response,'home.html')
         
